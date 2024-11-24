26c35747c76e2f16258139779e9a5e190d620a3a
Hruish <huangrui2004@outlook.com>
2024-11-25 01:19:47 +0800

    Site updated: 2024-11-25 01:19:47

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
     /** 计时起始时间，自行修改 **/
-    var start = new Date("2024/11/23 00:00:00");
+    var start = new Date("2024/11/13 00:00:00");
   
     function update() {
       var now = new Date();
